893eb9cfecf5ef7b7b8431597e88fe712d3ccb46
lawalhassan <ellhassan213@gmail.com>
2018-06-23 23:26:08 +0100

    trying node

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-diff /usr/src/app/arduino.ino /data/arduino.ino || PROGRAMMER=1
+diff /usr/src/app/arduino/arduino.ino /data/arduino/arduino.ino || PROGRAMMER=1
 if [ "${PROGRAMMER:-}" == "1" ]; then
   echo $PROGRAMMER
-  pushd /usr/src/app
+  pushd /usr/src/app/arduino
   make upload && cp arduino.ino /data/
   unset PROGRAMMER
   popd
 fi
 
-node server.js
+node /app/server.js

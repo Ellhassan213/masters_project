de85214619228524dc35a48a0cbeaff61d2787c7
lawalhassan <ellhassan213@gmail.com>
2018-06-23 20:16:39 +0100

    trying node

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
   socket.on('light', function(data) { //get light switch status from client
     lightvalue = data;
     if (lightvalue != LED.readSync()) { //only change LED if status has changed
-      LED.writeSync(lightvalue); //turn LED on or off
+      LED.writeSync(1); //turn LED on or off
       // var port = server.address().port;
       console.log('NOTHING HAPPENED!!');
     }

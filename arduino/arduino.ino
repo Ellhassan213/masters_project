0a2300fa473eb7e793c830539edcd47e5a2a4d95
lawalhassan <ellhassan213@gmail.com>
2018-06-25 16:40:12 +0100

    trying node

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -9,6 +9,16 @@
 #include <Adafruit_GFX.h>  // Core graphics library
 #include <RGBmatrixPanel.h> // Hardware-specific library
 
+#define CLK 8  // MUST be on PORTB! (Use pin 11 on Mega)
+#define OE  9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+#define D A3
+
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
+
 /*Global Variables */
 char char_in = -1;
 char data_in[100];

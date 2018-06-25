adb2af2e879f8ed25965d0f192314e1d69df53e6
lawalhassan <ellhassan213@gmail.com>
2018-06-25 01:23:02 +0100

    trying node

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include <Adafruit_GFX/Adafruit_GFX.h>   // Core graphics library
+#include "Adafruit_GFX.h"  // Core graphics library
 #include <RGBmatrixPanel.h> // Hardware-specific library
 
 /*Global Variables */

fbef7c336d17146169fc1bd2c7742599b49577dc
lawalhassan <ellhassan213@gmail.com>
2018-06-27 11:57:54 +0100

    Almost Almost there!

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,15 @@
+/* Start of Arduino Instructions */
+
+
+/* Include Files */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include <Adafruit_GFX.h>   // Core graphics library
 #include <RGBmatrixPanel.h> // Hardware-specific library
 
-// Pinout
+// Pinout for 32x32 Matrix - Arduino uno
 #define CLK 8  // MUST be on PORTB! (Use pin 11 on Mega)
 #define OE  9
 #define LAT 10
@@ -12,31 +20,71 @@
 
 #define clear() fillScreen(0)
 
+/*Global Variables */
+char char_in = -1;
+char data_in[100];
+int itr = 0;
+
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
-void rings(int count, double radius, double offset, int r, int g, int b){
+/* Function Pointers */
 
-  double theta = offset;
-  double delta = (2 * PI) / count;
-  double x = 0;
-  double y = 0;
-  
-  for(int i = 0; i < count; i++){
+int (*blinking_pointer)(double);
+//int (*align_pointer)(double);
+void (*middleBox_pointer)();
+void (*pixel_pointer)(double, double, double);
+void (*rings_pointer)(double, double, double, double, double);
 
-    x = radius * cos(theta) + (matrix.width() / 2);
-    y = radius * sin(theta) + (matrix.height() / 2);
+/* Functions */
 
-    matrix.drawPixel(x, y, matrix.Color333(r, g, b)); 
-    digitalWrite(11, HIGH);
-    delay(500);
+void rings(double count, double radius, double offset, double colour, double exposure){
 
-    matrix.drawPixel(x, y, 0);
-    digitalWrite(11, LOW);
-    delay(500);
-//    matrix.clear();
+    double delta = (2 * PI) / count;
+    double x = 0;
+    double y = 0;
+    double theta = 0;
     
-    theta += delta;    
-  }
+    int r = 0; int g = 0; int b = 0;
+
+    if(offset == 3){
+        theta = PI / 3;
+    }
+    else if(offset == 4){
+        theta = PI / 4;
+    }
+
+    if(colour == 01){
+        
+        r = 7; g = 0; b = 0;
+    }
+    else if(colour == 02){
+
+        r = 0; g = 7; b = 0;
+    }
+    else if(colour == 03){
+
+        r = 0; g = 0; b = 7;
+    }
+    else{
+
+        r = 7; g = 7; b = 7;
+    }
+    
+    for(int i = 0; i < count; i++){
+
+        x = radius * cos(theta) + (matrix.width() / 2);
+        y = radius * sin(theta) + (matrix.height() / 2);
+
+        matrix.drawPixel(x, y, matrix.Color333(r, g, b)); 
+        digitalWrite(11, HIGH);
+        delay(exposure);
+
+        matrix.drawPixel(x, y, 0);
+        digitalWrite(11, LOW);
+        delay(exposure);
+        
+        theta += delta;    
+    }
 }
 
 void middleBox(){
@@ -46,50 +94,142 @@ void middleBox(){
   matrix.drawPixel(15, 16, matrix.Color333(7, 7, 7));
   matrix.drawPixel(16, 16, matrix.Color333(7, 7, 7));
   matrix.drawPixel(16, 15, matrix.Color333(7, 7, 7));
-
-  delay(500);  
 }
 
-void align(int d){
+void align(double exposure){
 
   // Align
   matrix.drawPixel(16, 13, matrix.Color333(7, 7, 7));
-  delay(d);
+  delay(exposure);
   matrix.drawPixel(16, 19, matrix.Color333(7, 7, 7));
-  delay(d);
+  delay(exposure);
   matrix.drawPixel(13, 16, matrix.Color333(7, 7, 7));
-  delay(d);
+  delay(exposure);
   matrix.drawPixel(19, 16, matrix.Color333(7, 7, 7));
-  delay(d);  
+  delay(exposure);  
+
+  return 0;
+}
+
+int blinking(double exposure){
+    
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(exposure);              // wait for a second
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    delay(exposure);             // wait for a second
+
+    return 0;
+}
+
+void pixel(double x, double y, double colour){
+
+    int r = 0; int g = 0; int b = 0;
+
+    if(colour == 01){
+        
+        r = 7; g = 0; b = 0;
+    }
+    else if(colour == 02){
+
+        r = 0; g = 7; b = 0;
+    }
+    else if(colour == 03){
+
+        r = 0; g = 0; b = 7;
+    }
+    else{
+
+        r = 7; g = 7; b = 7;
+    }
+
+    matrix.drawPixel(x, y, matrix.Color333(r, g, b));
+}
+
+unsigned concatenate(unsigned first, unsigned second){
+
+    unsigned power = 10;
+    while(second >= power){
+        power *= 10;
+    }
+    return first * power + second;
+}
+
+int decoder(){
+
+    while(Serial.available() > 0){
+
+        char_in = Serial.read();
+        data_in[itr] = (char_in - '0');
+
+        Serial.write(char_in);
+        itr++;
+    }
+
+    double id = concatenate(data_in[0], data_in[1]);
+    
+    if(id == 01){
+
+        double e1 = concatenate(data_in[2], data_in[3]);
+        double e2 = concatenate(data_in[4], data_in[5]);
+
+        double exposure = concatenate(e1, e2);
+        (*blinking_pointer)(exposure);
+    }
+    else if(id == 02){
+
+        double e1 = concatenate(data_in[2], data_in[3]);
+        double e2 = concatenate(data_in[4], data_in[5]);
+
+        double exposure = concatenate(e1, e2);
+        align(exposure);
+    }
+    else if(id == 03){
+
+        (*middleBox_pointer)();
+    }
+    else if(id == 04){
+        
+        double count = concatenate(data_in[2], data_in[3]);
+        double radius = concatenate(data_in[4], data_in[5]);
+        double offset = concatenate(data_in[6], data_in[7]);
+        double colour = concatenate(data_in[8], data_in[9]);
+
+        double e1 = concatenate(data_in[10], data_in[11]);
+        double e2 = concatenate(data_in[12], data_in[13]);
+
+        double exposure = concatenate(e1, e2);
+
+        (*rings_pointer)(count, radius, offset, colour, exposure);
+    }
+
+  return 0;
 }
 
 
+
+/* Arduino Built-in Functions*/
+
 void setup(){
 
-  matrix.begin();
+    // initialise serial comms and matrix control
+    Serial.begin(9600); 
+    matrix.begin();
 
-  pinMode(11, OUTPUT);
-  digitalWrite(11, LOW);
+    // initialise function pointers
+    blinking_pointer = blinking;
+    //align_pointer = align;
+    middleBox_pointer = middleBox;
+    rings_pointer = rings;
+    pixel_pointer = pixel;
 
-//  rings(12, 10, 0, 0, 7, 0);
-//  rings(8, 8, PI/4, 0, 7, 0);
-  
+    // initialize digital pins
+    pinMode(13, OUTPUT);
 
-//  middleBox();
-
-//  align(0);
+    pinMode(11, OUTPUT);
+    digitalWrite(11, LOW);
 }
 
 void loop(){
 
-rings(8, 4, PI/3, 0, 7, 0);
-
-//  pinMode(13, OUTPUT);
-//  digitalWrite(13, HIGH);
-//  delay(500);
-//  digitalWrite(13, LOW);
-//  delay(500);
-//    rings(8, 4, PI/3, 0, 7, 0);
-//  rings(8, 8, PI/4, 7, 0, 0);
-//  rings(12, 10, 0, 0, 7, 0);
+    decoder();
 }

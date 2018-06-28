93f8be6f2a72582a7590fe3bccfd62c1772b991d
lawalhassan <ellhassan213@gmail.com>
2018-06-28 02:48:41 +0100

    Almost Almost there!

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -145,15 +145,14 @@ void rings(double count, double radius, double offset, double colour, double exp
     
     for(int i = 0; i < count; i++){
 
-        x = radius * cos(theta) + (matrix.width() / 2);
-        y = radius * sin(theta) + (matrix.height() / 2);
+        x = radius * cos(theta) + (32 / 2);
+        y = radius * sin(theta) + (32 / 2);
 
         matrix.drawPixel(x, y, matrix.Color333(r, g, b)); 
         digitalWrite(11, HIGH);
         delay(exposure);
 
-        // matrix.drawPixel(x, y, 0);
-        matrix.clear();
+        matrix.drawPixel(x, y, 0);
         digitalWrite(11, LOW);
         delay(exposure);
         

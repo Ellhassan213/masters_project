23e58ffb3e81efbf848462fab18cc29231ad410a
lawalhassan <ellhassan213@gmail.com>
2018-06-29 04:18:38 +0100

    Almost Almost there!

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -112,7 +112,7 @@ void rectangle(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t colour){
 
 void rings(int16_t count, int16_t  radius, int16_t  offset, uint16_t  colour, int16_t  exposure){
 
-    float delta = ( (2 * PI) / count ); // * (PI/180);
+    float delta = ( (2 * PI) / count );
     int16_t x = 0;
     int16_t y = 0;
     float theta = 0;
@@ -151,7 +151,7 @@ void rings(int16_t count, int16_t  radius, int16_t  offset, uint16_t  colour, in
         x = radius * cos(theta) + (16);
         y = radius * sin(theta) + (16);
 
-        matrix.drawPixel((int16_t)x, (int16_t)y, matrix.Color333(r, g, b)); 
+        matrix.drawPixel(x, y, matrix.Color333(r, g, b)); 
         digitalWrite(11, HIGH);
         delay(exposure);
 
@@ -159,7 +159,7 @@ void rings(int16_t count, int16_t  radius, int16_t  offset, uint16_t  colour, in
         digitalWrite(11, LOW);
         delay(exposure);
         
-        theta += delta; //* (180);    
+        theta += delta;    
     }
 
     // for(int i = 0; i < 10; i++){

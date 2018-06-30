9f7d73a390cd37dbad52e85630625dc070291551
lawalhassan <ellhassan213@gmail.com>
2018-06-30 04:04:22 +0100

    Almost Almost there!

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -309,15 +309,13 @@ void decoder(){
     }
     else if(id == 99){
 
-        int16_t nr = concatenate(data_in[2], data_in[3]);
+        int16_t count = concatenate(data_in[2], data_in[3]);
 
-        int16_t count = concatenate(data_in[4], data_in[5]);
-
-        int16_t e1 = concatenate(data_in[6], data_in[7]);
-        int16_t e2 = concatenate(data_in[8], data_in[9]);
+        int16_t e1 = concatenate(data_in[4], data_in[5]);
+        int16_t e2 = concatenate(data_in[6], data_in[7]);
         int16_t exposure = concatenate(e1, e2);
 
-        uint16_t colour = concatenate(data_in[10], data_in[11]);
+        uint16_t colour = concatenate(data_in[8], data_in[9]);
 
         int16_t r, g, b;
 
@@ -338,7 +336,7 @@ void decoder(){
             r = 7; g = 7; b = 7;
         }
 
-        for(int16_t i = 12; i <= count * 4; i + 4){
+        for(int16_t i = 10; i <= count * 4; i + 4){
 
             matrix.drawPixel(concatenate(data_in[i], data_in[i+1]), concatenate(data_in[i+2], data_in[i+3]), matrix.Color333(r, g, b));
         }
